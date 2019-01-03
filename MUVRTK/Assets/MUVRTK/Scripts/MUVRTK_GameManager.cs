60b5d62e7a2f52ec0379854846bf11387f3722fb
Kathi <Katharina.Ziolkowski@gmail.com>
2019-01-03 18:06:53 +0100

    Player Instantiation rework

--- MUVRTK/Assets/MUVRTK/Scripts/MUVRTK_GameManager.cs
+++ MUVRTK/Assets/MUVRTK/Scripts/MUVRTK_GameManager.cs
@@ -7,11 +7,24 @@
     using Photon.Pun;
     using Photon.Realtime;
 
+
     public class MUVRTK_GameManager : MonoBehaviourPunCallbacks
     {
         #region Public Fields
 
         public bool debug;
+
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
+
+        #endregion
+
+        #region Private Fields
+
+        private bool cameraLoaded;
+        private Camera mainCamera;
+        private GameObject instantiatedPlayer;
 
         #endregion
 
@@ -72,6 +85,42 @@
             // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
             PhotonNetwork.AutomaticallySyncScene = true;
 
+        }
+
+        private void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            }
+            else
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                if (mainCamera != null)
+                {
+                   instantiatedPlayer = PhotonNetwork.Instantiate(playerPrefab.name, mainCamera.transform.position, mainCamera.transform.rotation, 0);
+                }
+                else
+                {
+                    instantiatedPlayer = PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0,0,0), Quaternion.identity, 0);
+                }
+
+
+            }
+        }
+
+        private void Update()
+        {
+            if (!cameraLoaded)
+            {
+                if(mainCamera == null && Camera.main != null)
+                {
+                    mainCamera = Camera.main;
+                    instantiatedPlayer.transform.parent = mainCamera.transform;
+                    cameraLoaded = true;
+                }
+            }
         }
 
 
