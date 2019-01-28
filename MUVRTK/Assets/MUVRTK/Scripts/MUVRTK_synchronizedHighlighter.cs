69c11dc033b2c0c006b978f3ffbde3f432e299e5
kahzn <Katharina.Ziolkowski@gmail.com>
2019-01-28 12:17:08 +0100

    synchronized Highlighter-Script added.

---
+++ MUVRTK/Assets/MUVRTK/Scripts/MUVRTK_synchronizedHighlighter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace MUVRTK
+{
+    [RequireComponent(typeof(PhotonView))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class MUVRTK_synchronizedHighlighter : MonoBehaviour, IPunObservable
+    {
+        public bool debug;
+
+        private MeshRenderer renderer;
+
+        private void Start()
+        {
+            renderer = gameObject.GetComponent<MeshRenderer>();
+        }
+
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                stream.SendNext(renderer.material.color);
+
+                if (debug)
+                    Debug.Log(this.name + ": Stream is Writing this Color: " + renderer.material.color);
+            }
+            else
+            {
+                renderer.material.color = (Color)stream.ReceiveNext();
+                if (debug)
+                    Debug.Log(this.name + ": Stream is Receiving this Color: " + renderer.material.color);
+            }
+        }
+    }
+}
+
+
