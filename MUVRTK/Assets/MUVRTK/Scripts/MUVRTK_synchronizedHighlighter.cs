411d36d7feaf5d8883cc18f9eb1b76ec129b7f12
kahzn <Katharina.Ziolkowski@gmail.com>
2019-01-28 13:33:37 +0100

    bugfixing

--- MUVRTK/Assets/MUVRTK/Scripts/MUVRTK_synchronizedHighlighter.cs
+++ MUVRTK/Assets/MUVRTK/Scripts/MUVRTK_synchronizedHighlighter.cs
@@ -13,6 +13,8 @@ namespace MUVRTK
 
         private MeshRenderer renderer;
 
+        private Color color;
+
         private void Start()
         {
             renderer = gameObject.GetComponent<MeshRenderer>();
@@ -23,18 +25,28 @@ namespace MUVRTK
         {
             if (stream.IsWriting)
             {
-                stream.SendNext(renderer.material.color);
+                stream.SendNext(renderer.material.color.r);
+                stream.SendNext(renderer.material.color.g);
+                stream.SendNext(renderer.material.color.b);
+                stream.SendNext(renderer.material.color.a);
 
                 if (debug)
                     Debug.Log(this.name + ": Stream is Writing this Color: " + renderer.material.color);
             }
             else
             {
-                renderer.material.color = (Color)stream.ReceiveNext();
+                color.r = (float)stream.ReceiveNext();
+                color.g = (float)stream.ReceiveNext();
+                color.b = (float)stream.ReceiveNext();
+                color.a = (float)stream.ReceiveNext();
+
                 if (debug)
                     Debug.Log(this.name + ": Stream is Receiving this Color: " + renderer.material.color);
+
+                renderer.material.color = color;
             }
         }
+
     }
 }
 
