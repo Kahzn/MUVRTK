94fe7b6628746921c79e7028b92d127c05f6fd2d
Kathi <Katharina.Ziolkowski@gmail.com>
2019-02-04 19:01:19 +0100

    POI Manager added.

---
+++ MUVRTK/Assets/MUVRTK/Scripts/MUVRTK_PoiManager.cs
@@ -0,0 +1,30 @@
+﻿namespace MUVRTK
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Manages the lifespan and Behaviour of the POI.
+    /// The parent class MUVRTK_SceneObject contains different actions that can be triggered on object spawn.
+    /// <para>Created by Katharina Ziolkowski, 2019-02-04</para>
+    /// </summary>
+    public class MUVRTK_PoiManager : MUVRTK_SceneObject
+    {
+        [SerializeField]
+        private int lifeSpanInMinutes;
+
+        private MeshRenderer meshRenderer;
+
+        private void Start(){
+
+            meshRenderer = GetComponent<MeshRenderer>();
+
+        }
+
+
+    }
+
+}
+
+
