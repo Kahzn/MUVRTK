86ffd4a5ede14134638c8b1c578168d81dbe8551
Kathi <Katharina.Ziolkowski@gmail.com>
2019-02-19 12:50:24 +0100

    Fixed SharedAwareness Color Issue

--- MUVRTK/Assets/MUVRTK/Scripts/MUVRTK_SharedAwareness.cs
+++ MUVRTK/Assets/MUVRTK/Scripts/MUVRTK_SharedAwareness.cs
@@ -1,4 +1,6 @@
-﻿namespace MUVRTK
+﻿using UnityEngine.Serialization;
+
+namespace MUVRTK
 {
     using System.Collections;
     using System.Collections.Generic;
@@ -12,12 +14,24 @@
     /// </summary>
     public class MUVRTK_SharedAwareness : MUVRTK_SceneObject
     {
+        #region Private Fields
+        
+        [FormerlySerializedAs("highlightStrength")] 
+        [Range(0,1)]
+        [SerializeField] private float sharedAwarenessHighlightStrength;
+        
         private MeshRenderer renderer;
         private int viewerCount;
-
         private Color currentColor;
         private int viewId;
 
+        private float lastR;
+        private float lastG;
+        private float lastB;
+        
+        #endregion
+        
+        #region MonoBehaviour Callbacks
 
         private void OnEnable()
         {
@@ -39,7 +53,8 @@
                 if (debug)
                     Debug.Log(name + " : ViewCount : " + viewerCount);
                 currentColor = renderer.material.color;
-                PhotonView.Get(this).RPC("ChangeColor", RpcTarget.All, viewId,  currentColor.r + 0.2f, currentColor.g +0.2f, currentColor.b + 0.2f);
+
+                PhotonView.Get(this).RPC("MakeColorLighter", RpcTarget.All, viewId,  currentColor.r, currentColor.g , currentColor.b );
             }
         }
 
@@ -53,20 +68,105 @@
                     Debug.Log(name + " : ViewCount : " + viewerCount);
 
                 currentColor = renderer.material.color;
-                PhotonView.Get(this).RPC("ChangeColor", RpcTarget.All, viewId, currentColor.r -0.2f, currentColor.g -0.2f, currentColor.b -0.2f);
+                PhotonView.Get(this).RPC("MakeColorDarker", RpcTarget.All, viewId, currentColor.r, currentColor.g, currentColor.b);
             }
 
         }
 
+        #endregion
+       
+        #region Pun RPCs
+        
         [PunRPC]
-        public void ChangeColor(int photonView, float r, float g, float b)
+        public void MakeColorLighter(int photonView, float r, float g, float b)
         {
             if(photonView == viewId)
             {
-                renderer.material.color = new Color(r, g, b, 1f);
+                //if you're reaching the limit, save the difference and add only that.
+                if (r + sharedAwarenessHighlightStrength > 1f)
+                {
+                    lastR = 1 - r;
+                    
+                    Debug.Log("r if");
+                }
+                else
+                {
+                    lastR = sharedAwarenessHighlightStrength;
+                    Debug.Log("r else");
+                }
+                
+                if (g + sharedAwarenessHighlightStrength > 1f)
+                {
+                    lastG = 1 - g;
+                    Debug.Log("g if");
+                }
+                else
+                {
+                    lastG = sharedAwarenessHighlightStrength;
+                    Debug.Log("g else");
+                }
+                
+                if (b + sharedAwarenessHighlightStrength > 1f)
+                {
+                    lastB = 1 - b;
+                    Debug.Log("b if");
+                }
+                else
+                {
+                    lastB = sharedAwarenessHighlightStrength;
+                    Debug.Log("b else");
+                }
+
+                renderer.material.color = new Color(r+lastR, g+lastG, b+lastB, 1f);
 
             }
         }
+        
+        [PunRPC]
+        public void MakeColorDarker(int photonView, float r, float g, float b)
+        {
+            if(photonView == viewId)
+            {
+                if (r - sharedAwarenessHighlightStrength < 0f)
+                {
+                    lastR = r;
+                    Debug.Log("r if");
+                }
+                else
+                {
+                    lastR = sharedAwarenessHighlightStrength;
+                    Debug.Log("r else");
+                    
+                }
+                
+                if (g - sharedAwarenessHighlightStrength < 0f)
+                {
+                    lastG = g;
+                    Debug.Log("g if");
+                }
+                else
+                {
+                    lastG = sharedAwarenessHighlightStrength;
+                    Debug.Log("g else");
+                }
+                
+                if (b - sharedAwarenessHighlightStrength < 0f)
+                {
+                    lastB = b;
+                    Debug.Log("b if");
+                }
+                else
+                {
+                    lastB = sharedAwarenessHighlightStrength;
+                    Debug.Log("b else");
+                }
+                
+                renderer.material.color = new Color(r - lastR, g - lastG, b - lastB, 1f);
+
+            }
+        }
+        
+        #endregion
     }
 }
 
